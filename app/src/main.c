b54b048f0955c38fe81f762e4562f6e1fbbf1053
LuSkywalker <callumlu@gmail.com>
2023-05-25 00:31:02 +0800

    [M] smooth breathing led with user define led

--- app/src/main.c
+++ app/src/main.c
@@ -31,27 +31,38 @@ int main(void)
 
     usr_led_init(LED3);
     usr_led_init(LED4);
+    usr_led_init(USR_LED);
 
     usr_btn_init(BTN_USER);
 
-    usr_led_on(LED3);
-    usr_led_off(LED4);
+    // usr_led_on(LED3);
+    // usr_led_off(LED4);
 
     DBG_MSG("press button to change led pulse speed...\r\n");
     for ( ; ; ) {
         led_spd_t curLedSpd = (usr_led_get_spd() * 2 + 1) * 10;
+        int32_t pulseSec = (DUTY_BASE / curLedSpd) * PWM_CONTINUE_MS;
         if (preLedSpd != curLedSpd) {
-            DBG_MSG("pulse per %d ms...\r\n", (int)(DUTY_BASE / curLedSpd) * PWM_CONTINUE_MS);
+            DBG_MSG("pulse per %d ms...\r\n", (int)pulseSec);
         }
 
         for (int i = 0; i <= DUTY_BASE; i += curLedSpd * 2) {
-            usr_led_pwm(LED3, PWM_FREQ, i);
+            usr_led_pwm(USR_LED, PWM_FREQ, i);
             //usr_led_pwm(LED4, PWM_FREQ, i);
         }
-        for (int i = DUTY_BASE; i >= 0; i -= curLedSpd * 2) {
-            usr_led_pwm(LED3, PWM_FREQ, i);
+
+        /* still for full scale */
+        usr_led_on(USR_LED);
+        util_delay_ms(pulseSec / 4);
+
+        for (int i = DUTY_BASE; i >= 0; i -= curLedSpd) {
+            usr_led_pwm(USR_LED, PWM_FREQ, i);
             //usr_led_pwm(LED4, PWM_FREQ, i);
         }
+
+        /* still for empty scale */
+        usr_led_off(USR_LED);
+        util_delay_ms(pulseSec / 5);
 
         preLedSpd = curLedSpd;
     }
